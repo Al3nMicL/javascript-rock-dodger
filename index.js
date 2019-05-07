4c27ef52691c308a1c9a7b7e60c8c2635e1f1f56
Al3nMicL <https://twitter.com/Al3nMicL>
2019-05-06 21:27:44 -0400

    checkCollision(rock) tests pass

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ var gameInterval = null
  */
 
 function checkCollision(rock) {
-  // implement me!
   // use the comments below to guide you!
   const top = positionToInteger(rock.style.top)
 
@@ -28,25 +27,29 @@ function checkCollision(rock) {
   if (top > 360) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
-    // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = 0;
+    // FIXED: The DODGER is 40 pixels wide -- how do we get the right edge?
+    const dodgerRightEdge = positionToInteger(DODGER.style.left) + 40;
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
-    // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = 0;
+    // FIXED: The rock is 20 pixel's wide -- how do we get the right edge?
+    const rockRightEdge = positionToInteger(rock.style.left) + 20;
+    // Think about it -- what's happening here?
 
-    if (false /**
-               * Think about it -- what's happening here?
-               * There's been a collision if one of three things is true:
-               * 1. The rock's left edge is < the DODGER's left edge,
-               *    and the rock's right edge is > the DODGER's left edge;
-               * 2. The rock's left edge is > the DODGER's left edge,
-               *    and the rock's right edge is < the DODGER's right edge;
-               * 3. The rock's left edge is < the DODGER's right edge,
-               *    and the rock's right edge is > the DODGER's right edge
-               */) {
-      return true
+    // There's been a collision if one of three things is true:
+    if (rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge) {
+      // 1. The rock's left edge is < the DODGER's left edge and the rock's right edge is > the DODGER's left edge;
+      return true;
+    } 
+
+    if (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) {
+      // 2. The rock's left edge is > the DODGER's left edge and the rock's right edge is < the DODGER's right edge;
+      return true;
+    }
+
+    if (rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge) {
+      // 3. The rock's left edge is < the DODGER's right edge and the rock's right edge is > the DODGER's right edge
+      return true;
     }
   }
 }
